f1cf95734390a976fdad42aa0ddff1ca497cb0dc
liyongbin <1103782715@qq.com>
2021-01-27 17:58:21 +0800

    完善模板2021-1-27

---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+import { headerWrapper } from './style'
+export default memo(function header() {
+  return (
+    <headerWrapper>
+      header
+    </headerWrapper>
+  )
+})
